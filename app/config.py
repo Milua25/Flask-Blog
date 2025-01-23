3be881c521b324fc1921a7425ebd875f6e2d6a8a
Ayo-milua <ademiluaayo94@gmail.com>
2025-01-23 18:00:28 -0500

    updated config.py

--- app/config.py
+++ app/config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     # configure the SQLite database, relative to the app instance folder
-    SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or  "sqlite:///users.db"
